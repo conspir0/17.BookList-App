efc1f450a874a544c07b0ba6524e7e49d5dbb643
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-01-08 21:36:31 +0100

    Create showAlert(message, className) with Vanish in 3 sec, Add Validate to Event: add a book. Add success message to add, remove a book

--- js/script.js
+++ js/script.js
@@ -49,6 +49,18 @@ class UI {
         }
     }
     
+    static showAlert(message, className) {
+        const div = document.createElement('div');
+        div.className = `alert alert-${className}`;
+        div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div, form);
+        
+        // Vanish in 3 seconds
+        setTimeout(() => document.querySelector('.alert').remove(), 3000);
+    }
+    
     static clearFields() {
         document.querySelector('#title').value = '';
         document.querySelector('#author').value = '';
@@ -72,17 +84,28 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
     const author = document.querySelector('#author').value;
     const isbn = document.querySelector('#isbn').value;
     
-    // Instatiate book
-    const book = new Book(title, author, isbn);
+    // Validate
+    if(title === '' || author === '' || isbn === '') {
+       UI.showAlert('Please fill in all fields', 'danger');
+       } else {
+        // Instatiate book
+        const book = new Book(title, author, isbn);
 
-    // Add bookt to UI
-    UI.addBookToList(book);
-
-    // Clear fileds
-    UI.clearFields();
+        // Add bookt to UI
+        UI.addBookToList(book);
+           
+        // Show success message
+        UI.showAlert('Book added', 'success');
+           
+        // Clear fileds
+        UI.clearFields();    
+       }
 });
 
 // Event: remove a book
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target);
-})
+    
+    // Show success message
+    UI.showAlert('Book removed', 'success');       
+});
